571d91f12e4a8e672023580a2a684c7ab7be6723
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-29 20:16:07 +0545

    Media Setup Done

--- backend/as_project/as_app/models.py
+++ backend/as_project/as_app/models.py
@@ -4,6 +4,7 @@ from django.core.validators import MinValueValidator
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
