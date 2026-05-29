4be6d6f65fdf74cb841db6b352584b73dabc57a2
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-29 21:22:58 +0545

    Image Url Fixes in Backend at Serializers for ease Frontend

--- backend/as_project/as_app/serializers.py
+++ backend/as_project/as_app/serializers.py
@@ -2,6 +2,22 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    
+    profile_picture = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
         fields = "__all__"
+        
+    def get_profile_picture(self, obj):
+        request = self.context.get('request')
+
+        if obj.profile_picture:
+            url = obj.profile_picture.url
+
+            if request:
+                return request.build_absolute_uri(url)
+
+            return url
+
+        return None
