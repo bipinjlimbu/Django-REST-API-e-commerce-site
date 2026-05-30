e255f47969f78a4d7335b841f78ea0af7c4df97c
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-30 22:12:46 +0545

    Single Category Url Added

--- backend/as_project/as_app/urls.py
+++ backend/as_project/as_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main_view import profile_view, category_view
+from .views.main_view import profile_view, category_view, single_category_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('profile/<int:user_id>/', profile_view, name='profile'),
     path('category/', category_view, name='category'),
+    path('category/<int:category_id>/', single_category_view, name='single_category'),
 ]
