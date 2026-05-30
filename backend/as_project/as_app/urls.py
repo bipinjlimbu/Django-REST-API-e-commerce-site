06d5ad6bc82950b53b77e5e03d9d9531fc2e67a9
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-30 21:49:13 +0545

    Category View Url Added

--- backend/as_project/as_app/urls.py
+++ backend/as_project/as_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main_view import profile_view
+from .views.main_view import profile_view, category_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/<int:user_id>/', profile_view, name='profile'),
+    path('category/', category_view, name='category'),
 ]
