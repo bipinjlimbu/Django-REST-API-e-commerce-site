1292674cd1cfb483bfbf461f69bb4ab7b4ab9dd4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-31 19:31:51 +0545

    Users Url Added

--- backend/as_project/as_app/urls.py
+++ backend/as_project/as_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main_view import single_profile_view, category_view, single_category_view
+from .views.main_view import user_view, single_profile_view, category_view, single_category_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('users/', user_view, name='user'),
     path('profile/<int:user_id>/', single_profile_view, name='single_profile'),
     path('category/', category_view, name='category'),
     path('category/<int:category_id>/', single_category_view, name='single_category'),
