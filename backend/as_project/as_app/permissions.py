f5c1fa5f5d2715a4ee1148478f5842ad01e55be9
bipinjlimbu <bipinlimbu712@gmail.com>
2026-06-03 19:32:53 +0545

    Custom Permission IsAdminOrReadOnly Added

--- backend/as_project/as_app/permissions.py
+++ backend/as_project/as_app/permissions.py
@@ -1,9 +1,9 @@
 from rest_framework.permissions import BasePermission
 
-class IsAdminOrReadOnlyAuthenticated(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'GET':
-            return request.user and request.user.is_authenticated
+            return True
         
         if request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
             return request.user and request.user.is_authenticated and request.user.is_staff
